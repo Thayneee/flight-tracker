e908c6b9adc4aeaa7296b399fbb1bea7ec0a850c
Thayneee <thayne.dean@siu.edu>
2023-03-21 18:58:27 -0500

    made json destination formal, exception if no data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ I2C_NUM_COLS = 20
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 while True:
-    with open("../../../run/dump1090-mutability/aircraft.json", "r") as f:
+    with open("/run/dump1090-mutability/aircraft.json", "r") as f:
         data = json.load(f)
 
     required_fields = ['squawk', 'flight', 'altitude', 'speed']
@@ -30,15 +30,21 @@ while True:
         else:
             continue
 
-
-    for i in range(len(squawk)):
+    if len(squawk) == 0:
+        lcd.clear()
         lcd.move_to(0,0)
-        lcd.putstr(f"Flight {flight[i]} is")
+        lcd.putstr("There are no planes above.")
         lcd.move_to(0,1)
-        lcd.putstr(f"{altitude[i]} feet above")
-        lcd.move_to(0,2)
-        lcd.putstr(f"right now! Going {speed[i]}")
-        lcd.move_to(0,3)
-        lcd.putstr(f"knots. Squawk: {squawk[i]}")
-        time.sleep(3)
+        lcd.putstr(":(")
+    else:
+        for i in range(len(squawk)):
+            lcd.move_to(0,0)
+            lcd.putstr(f"Flight {flight[i]} is")
+            lcd.move_to(0,1)
+            lcd.putstr(f"{altitude[i]} feet above")
+            lcd.move_to(0,2)
+            lcd.putstr(f"right now! Going {speed[i]}")
+            lcd.move_to(0,3)
+            lcd.putstr(f"knots. Squawk: {squawk[i]}")
+            time.sleep(3)
 

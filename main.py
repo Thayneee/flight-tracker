48364d273beb53fc7d2b8b6a00ebb3b1366d2de1
Thayneee <thayne.dean@siu.edu>
2023-03-20 23:52:33 -0500

    Added actual destination for the json.

--- main.py
+++ main.py
@@ -9,37 +9,36 @@ I2C_NUM_COLS = 20
 
 lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-with open("aircraft.json", "r") as f:
-    data = json.load(f)
+while True:
+    with open("../../../run/dump1090-mutability/aircraft.json", "r") as f:
+        data = json.load(f)
 
-required_fields = ['squawk', 'flight', 'altitude', 'speed']
+    required_fields = ['squawk', 'flight', 'altitude', 'speed']
 
-squawk = []
-flight = []
-altitude = []
-speed = []
+    squawk = []
+    flight = []
+    altitude = []
+    speed = []
 
-for aircraft in data["aircraft"]:
-    if all(field in aircraft for field in required_fields):
-        squawk.append(aircraft["squawk"])
-        flight.append(aircraft["flight"])
-        altitude.append(aircraft["altitude"])
-        speed.append(aircraft["speed"])
+    for aircraft in data["aircraft"]:
+        if all(field in aircraft for field in required_fields):
+            squawk.append(aircraft["squawk"])
+            flight.append(aircraft["flight"])
+            altitude.append(aircraft["altitude"])
+            speed.append(aircraft["speed"])
 
-    else:
-        continue
+        else:
+            continue
 
-#for i in range(len(squawk)):
-#    print(squawk[i], flight[i], altitude[i], speed[i])
 
-for i in range(len(squawk)):
-    lcd.move_to(0,0)
-    lcd.putstr(f"Flight {flight[i]} is")
-    lcd.move_to(0,1)
-    lcd.putstr(f"{altitude[i]} feet above")
-    lcd.move_to(0,2)
-    lcd.putstr(f"right now! Going {speed[i]}")
-    lcd.move_to(0,3)
-    lcd.putstr(f"knots. Squawk: {squawk[i]}")
-    time.sleep(3)
+    for i in range(len(squawk)):
+        lcd.move_to(0,0)
+        lcd.putstr(f"Flight {flight[i]} is")
+        lcd.move_to(0,1)
+        lcd.putstr(f"{altitude[i]} feet above")
+        lcd.move_to(0,2)
+        lcd.putstr(f"right now! Going {speed[i]}")
+        lcd.move_to(0,3)
+        lcd.putstr(f"knots. Squawk: {squawk[i]}")
+        time.sleep(3)
 

b75b7f7b20fb479748326915026afd40d56fd8f2
Thayneee <thayne.dean@siu.edu>
2023-03-20 23:29:02 -0500

    Updated code to test lcd display.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import json
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+
+I2C_ADDR = 0x27
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+
+lcd = I2cLcd(1, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 with open("aircraft.json", "r") as f:
     data = json.load(f)
@@ -24,4 +32,9 @@ for aircraft in data["aircraft"]:
 #    print(squawk[i], flight[i], altitude[i], speed[i])
 
 
+lcd.putstr(f"Flight {flight[0]} is {altitude[0]} feet")
+lcd.move_to(0,1)
+lcd.putstr(f"above right now! Going {speed[0]} knots.")
+lcd.move_to(0,2)
+lcd.putstr(f"It has the squawk {squawk[0]}")
 
